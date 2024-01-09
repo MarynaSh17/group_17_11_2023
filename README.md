2652c7e0c25f4020521d573b0149a864cfbb0498
Maryna Shovkova <marinka685m16@gmail.com>
2024-01-09 21:36:53 +0000

    add first file

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # group_17_11_2023
+- Maryna Shovkova
+- 17.11.2023
---
+++ first_homework.py
@@ -0,0 +1,2 @@
+favorite_city = "Kyiv is the capital and most populous city of Ukraine.It is in north-central Ukraine along the Dnieper River."
+print(favorite_city)
