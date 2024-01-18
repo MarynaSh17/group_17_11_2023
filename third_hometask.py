8720755916a0bfdfc431592efc7f2b14a6aa4ce7
Maryna Shovkova <marinka685m16@gmail.com>
2024-01-18 21:53:32 +0000

    add second file

--- third_hometask.py
+++ third_hometask.py
@@ -1,8 +1,7 @@
 city = "6..58київ\nоДеса     Львів\tжитоМИР      уЖгОрОд ХарКІВ       слАвУтИч74$:?$"
 change_format_city = city.strip('6..58 74$:?$')
-cities = change_format_city.split()
-print(cities)
-result = ['київ', 'оДеса', 'Львів', 'житоМИР', 'уЖгОрОд', 'ХарКІВ', 'слАвУтИч']
-for cities_result in result:
-    lower_cities = cities_result.lower().capitalize()
-    print(f"Я\U00002764_{lower_cities}_")
+cities = change_format_city.lower().title().split()
+for cities_result in cities:
+    if cities_result[-1] == 'а':
+        cities_result = f"{cities_result.strip('а')}у"
+    print(f"Я\U00002764 {cities_result} ")
